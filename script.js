0e88dafd73c7a918d968de393f1e73d5104194cc
lionand <landronov.97@gmail.com>
2019-11-18 21:50:50 +0700

    bug fixes

--- script.js
+++ script.js
@@ -1,23 +1,26 @@
 // https://developer.mozilla.org/en-US/docs/Web/API/Window/DOMContentLoaded_event
 // https://developer.mozilla.org/en-US/docs/Glossary/IIFE
+// https://learn.javascript.ru/introduction-browser-events
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
 var db = []
 
 export function addMeal(event) {
     event.preventDefault()
 
     var id = db.push({
-        meal: event.target.meal.value, 
+        no: db.length + 1,
+        meal: event.target.meal.value,
         calories: event.target.calories.value
-    })
+    }) - 1;
 
-    render(id, db[id - 1])
+    render(db[id])
 }
 
-function render(id, { meal, calories }) {
+function render({ no, meal, calories }) {
     var tr = document.createElement('tr')
 
     tr.innerHTML = `
-        <td>${id}</td>
+        <td>${no}</td>
         <td>${meal}</td>
         <td>${calories}</td>
     `
